a961cc3d17470967e6889602f3717c75f445659f
developer <sinsnittau@hotmail.com>
2022-12-24 08:34:37 +0800

    encrypt

--- src/hill/hill.java
+++ src/hill/hill.java
@@ -2,4 +2,26 @@ package hill;
 
 public class hill {
 
+	public static String encrypt(String plaintext, int shift) {
+	    StringBuilder ciphertext = new StringBuilder();
+	    for (char c : plaintext.toCharArray()) {
+	      if (Character.isLetter(c)) {
+	        if (Character.isUpperCase(c)) {
+	          int asciiValue = (int) c;
+	          int newAsciiValue = ((asciiValue - 65 + shift) % 26) + 65;
+	          char newChar = (char) newAsciiValue;
+	          ciphertext.append(newChar);
+	        } else {
+	          int asciiValue = (int) c;
+	          int newAsciiValue = ((asciiValue - 97 + shift) % 26) + 97;
+	          char newChar = (char) newAsciiValue;
+	          ciphertext.append(newChar);
+	        }
+	      } else {
+	        ciphertext.append(c);
+	      }
+	    }
+	    return ciphertext.toString();
+	  }
+	
 }
